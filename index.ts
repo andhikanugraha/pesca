0544fafb7337d185494dda847734a127b1d9cab5
Andhika Nugraha <andhikanugraha@users.noreply.github.com>
2025-03-09 10:05:49 +0800

    Moved lib/main.ts to lib/operator.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
-import main from "./lib/main.ts";
+import { resolve } from "@std/path";
+import { parse } from "@std/toml";
+import { createOperator } from "./lib/operator.ts";
+import task from "tasuku";
 
 main("pesca.toml");
+
+async function main(pathToConfigToml = "pesca.toml") {
+  pathToConfigToml = resolve(Deno.cwd(), pathToConfigToml);
+  let unresolvedConfig: Record<string, unknown> = {};
+
+  await task(`Reading ${pathToConfigToml}`, async () => {
+    const tomlString = await Deno.readTextFile(pathToConfigToml);
+    unresolvedConfig = parse(tomlString);
+  });
+
+  const operator = await createOperator(unresolvedConfig);
+
+  while (confirm('\nPull?')) {
+    await operator.pull();
+  }
+}
