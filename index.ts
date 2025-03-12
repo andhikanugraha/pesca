e49b0c6eb657954ca795ddc3349786814e2d81c2
Andhika Nugraha <andhikanugraha@users.noreply.github.com>
2025-03-12 11:17:23 +0800

    Use pesca.yml instead of pesca.toml

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 import { resolve } from "@std/path";
-import { parse } from "@std/toml";
+import { parse } from "@std/yaml";
 import { createOperator } from "./lib/operator/operator.ts";
 import createServer from "./lib/server/server.tsx";
 import task from "tasuku";
 import { createScheduler } from "./lib/scheduler.ts";
 import { type Config, resolveConfig } from "./lib/config.ts";
 
-async function main(pathToConfigToml = "pesca.toml") {
-  pathToConfigToml = resolve(Deno.cwd(), pathToConfigToml);
+async function main(pathToConfigYaml = "pesca.yml") {
+  pathToConfigYaml = resolve(Deno.cwd(), pathToConfigYaml);
   let unresolvedConfig: Record<string, unknown> = {};
 
-  await task(`Reading ${pathToConfigToml}`, async () => {
-    const tomlString = await Deno.readTextFile(pathToConfigToml);
-    unresolvedConfig = parse(tomlString);
+  await task(`Reading ${pathToConfigYaml}`, async () => {
+    const tomlString = await Deno.readTextFile(pathToConfigYaml);
+    unresolvedConfig = parse(tomlString) as Record<string, unknown>;
   });
 
   const config = (await task(
